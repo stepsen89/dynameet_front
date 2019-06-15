f40bd8ee2b8913fb2422f16f7c0de23da577bec3
[stepsen89] <stephanie.senoner@gmail.com>
2019-06-15 12:14:41 +0100

    setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-library.add(fas);
-
 @NgModule({
   declarations: [
     AppComponent
@@ -16,7 +11,6 @@ library.add(fas);
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
