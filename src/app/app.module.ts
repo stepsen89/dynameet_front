01e7267729f7a04bf217a181fdac2bd5e8e2c18d
[stepsen89] <stephanie.senoner@gmail.com>
2019-06-15 14:01:00 +0100

    added header to layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,11 +22,12 @@ import { environment } from '../environments/environment';
     LayoutComponent,
     HeaderComponent,
     FooterComponent,
-    AngularFireModule
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
