aaf0bc930a373f771338cd95f742339e7c4f51a9
[stepsen89] <stephanie.senoner@gmail.com>
2019-06-15 15:29:53 +0100

    landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingComponent } from './pages/landing/landing.component';
+import { MainComponent } from './main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: 'home', 
+    component: LandingComponent 
+  },
+  { 
+    path: 'main', 
+    component: MainComponent 
+  },
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  // { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
