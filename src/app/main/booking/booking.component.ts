86758311f48076de9df1ee8a90df6cf3fc33c653
[stepsen89] <stephanie.senoner@gmail.com>
2019-06-16 11:41:42 +0100

    connection to firebase update

--- src/app/main/booking/booking.component.ts
+++ src/app/main/booking/booking.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
 
 @Component({
   selector: 'app-booking',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./booking.component.scss']
 })
 export class BookingComponent implements OnInit {
+  showModal: Boolean = false;
 
-  constructor() { }
+  constructor(
+    public db: AngularFireDatabase
+  ) { }
 
   ngOnInit() {
   }
 
+  showModalOnScreen(){
+    console.log("jljl");
+    this.showModal ? this.showModal = false : this.showModal = true;
+    console.log(this.showModal)
+  }
+
+  removeListItem(){
+    const itemRef = this.db.object('roomBookings/room1');
+    itemRef.update({'06-15-2019:19:00:00': "booked"});
+    console.log(itemRef);
+  }
 }
